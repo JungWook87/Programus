305028ee6222f3c89d87e179360598df51f7ca72
JungWook87 <kjo6391@gmail.com>
2022-12-12 10:19:10 +0900

    progrmus

---
+++ src/programus/lv00/옹알이.java
@@ -0,0 +1,21 @@
+package programus.lv00;
+
+public class 옹알이 {
+	
+	public static String[] babblingArray = {"aya", "ye", "woo", "ma"};
+	
+	public int solution(String[] babbling) {
+		int answer = 0;
+		
+		for(String bab : babbling) {
+			for(String str : babblingArray) {
+				bab = bab.replaceFirst(str, " ");
+			}
+			if(bab.replaceAll(" ", "").equals("")){
+				answer++;
+			}
+		}
+		
+		return answer;
+	}
+}
---
+++ src/programus/lv00/옹알이실행.java
@@ -0,0 +1,14 @@
+package programus.lv00;
+
+public class 옹알이실행 {
+
+	public static void main(String[] args) {
+	
+		옹알이 st = new 옹알이();
+		
+		String[] bab = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+		int result = st.solution(bab);
+		System.out.println(result);
+	}
+
+}
